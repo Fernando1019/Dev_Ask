685882412bd4c962185dee1e9dfcbc7f02457c56
Fernando Ambrosio <fernandoam_1019@hotmail.com>
2016-04-16 01:36:25 -0600

    Inicio

---
+++ app/src/main/java/com/example/fernandoambrosio/devask/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.fernandoambrosio.devask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.entrada);
+    }
+}
