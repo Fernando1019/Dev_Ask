8d3586fad04dffb541102b46a0e7c5742b7deab6
chaqui <josue.chaqui@gmail.com>
2016-04-26 03:02:13 -0600

    agregando logica al frontEnd

--- app/src/main/java/com/example/fernandoambrosio/devask/PreguntaFv.java
+++ app/src/main/java/com/example/fernandoambrosio/devask/PreguntaFv.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.fernandoambrosio.devask.tipos.PreguntaDirectaTipo;
 import com.example.fernandoambrosio.devask.tipos.PreguntaOpcionMultiple;
 import com.example.fernandoambrosio.devask.tipos.PreguntaVF;
+
+import org.w3c.dom.Text;
 
 import java.util.Random;
 
@@ -15,48 +19,55 @@ import java.util.Random;
  * Created by Fernando Ambrosio on 23/04/2016.
  */
 public class PreguntaFv extends AppCompatActivity {
-
+    private TextView preguntaFV;
+    private Button botonFalso;
+    private  Button botonVerdadero;
+    private String respuesta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.preguntafv);
+        preguntaFV = (TextView) this.findViewById(R.id.tvPreguntaFv);
+        botonFalso = (Button) this.findViewById(R.id.buttonF);
+        botonVerdadero = (Button) this.findViewById(R.id.buttonV);
+        Bundle bundle = getIntent().getExtras();
+        preguntaFV.setText(bundle.getString("pregunta"));
+        respuesta= bundle.getString("respuesta");
     }
 
     public  void jugar() {
         Juego juego = new Juego(this);
         Random random = new Random();
+        Intent intent = null;
         int numero = random.nextInt(3) + 1;
 
         if (numero == 1) {
             PreguntaVF vf = juego.crearPreguntaVf();
-            Intent intent = new Intent(PreguntaFv.this, PreguntaFv.class);
+            intent = new Intent(this, PreguntaFv.class);
             intent.putExtra("pregunta",vf.getContexto());
             intent.putExtra("respuesta",vf.getRespuesta());
-            startActivity(intent);
-            finish();
         }
         if (numero == 2) {
             PreguntaDirectaTipo directa = juego.crearPreguntaDirecta();
-            Intent intent = new Intent(PreguntaFv.this, PreguntaDirecta.class);
+            intent = new Intent(this, PreguntaDirecta.class);
             intent.putExtra("pregunta",directa.getContexto());
             intent.putExtra("pregunta",directa.getRespuesta());
-            startActivity(intent);
-            finish();
         }
         if (numero == 3) {
             PreguntaOpcionMultiple multiple = juego.crearPreguntaOpcionMultiple();
-            Intent intent = new Intent(PreguntaFv.this, PreguntaSeleccion.class);
+            intent = new Intent(this, PreguntaSeleccion.class);
             String[] respuestas = multiple.getRespuesta();
             intent.putExtra("pregunta",multiple.getContexto());
             intent.putExtra("respuesta1",respuestas[0]);
             intent.putExtra("respuesta2",respuestas[1]);
             intent.putExtra("respuesta3",respuestas[2]);
             intent.putExtra("correcta",multiple.getCorrecta());
-            startActivity(intent);
-            finish();
+
         }
+        startActivity(intent);
+        finish();
     }
 }
 
